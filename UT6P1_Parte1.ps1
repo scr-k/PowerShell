6643bdbf8800f3c82beb01008653560de93816c5
scr-k <118215771+scr-k@users.noreply.github.com>
2026-01-30 11:37:57 +0100

    UT6P1 --> AD, OU, Usuarios y Grupos --> DOS PARTES
    Se deben ejecutar ambos scripts para completar el ejercicio.
    El primero crea el AD, cambia el nombre del equipo, etc... Después de todo eso, requiere reinicio
    
    DESPUÉS de reiniciar, se puede ejecutar el SEGUNDO script donde se crean las OU y se importan los datos de los CSV

---
+++ UT6P1_Parte1.ps1
@@ -0,0 +1,77 @@
+﻿# UT6 - Práctica 1 --> Crear estructura AD con script de PowerShell
+# Izhar Sedano - 2º ASIR 2025-2026
+
+# Este script debe ejecutarse en 2 fases. Este es el script 1 de 2
+# Se debe hacer así ya que hay un reinicio obligatorio después de instalar AD
+
+# ==================================================================================
+
+#####      *****      SCRIPT 1      *****      #####
+# !!! SE DEBE EJECUTAR COMO ADMINISTRADOR !!! #
+
+# ==================================================================================
+
+Write-Host " --- INICIANDO FASE 1: CONFIGURACIÓN DEL EQUIPO Y AD --- " -ForegroundColor Cyan
+
+# ==================================================================================
+
+# 1.1. Cambio de nombre del equipo
+Write-Host "Cambiando nombre del equipo a Empresa-DC..."
+Rename-Computer -NewName "Empresa-DC" -Force
+Write-Host "Nombre cambiado correctamente"
+
+# ==================================================================================
+
+# 1.2. Instalación de Active Directory
+Write-Host "Instalando el rol de Active Directory Domain Services..."
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+# ==================================================================================
+
+# 1.3. Configuración del Bosque
+$domainName = "empresa.local"
+
+#    La contraseña debe ser cifrada para que se pueda aplicar:
+$securePass = ConvertTo-SecureString "aso.2025" -AsPlainText -Force
+
+Write-Host "Promocionando a Controlador de Dominio (empresa.local)..."
+Import-Module ADDSDeployment
+Install-ADDSForest `
+    -CreateDnsDelegation:$false `
+    -DatabasePath "C:\Windows\NTDS" `
+    -DomainMode "WinThreshold" `
+    -DomainName $domainName `
+    -DomainNetbiosName "EMPRESA" `
+    -ForestMode "WinThreshold" `
+    -InstallDns:$true `
+    -LogPath "C:\Windows\NTDS" `
+    -NoRebootOnCompletion:$true `
+    -SysvolPath "C:\Windows\SYSVOL" `
+    -Force:$true `
+    -SafeModeAdministratorPassword $securePass
+
+# ==================================================================================
+
+# 1.4. Aviso para poder reiniciar y aplicar cambios
+
+Write-Host "***************************************************************" -ForegroundColor Red
+Write-Host " ATENCIÓN: El equipo DEBE REINICIARSE para aplicar los cambios." -ForegroundColor Red
+Write-Host " Tras el reinicio, EJECUTE el script 'UT6P1_Parte2.ps1'." -ForegroundColor Red
+Write-Host "***************************************************************" -ForegroundColor Red
+
+Start-Sleep -Seconds 5
+
+# ==================================================================================
+
+# A continuación, el equipo debe reiniciarse.
+# Al volver a iniciar sesión, se debe ejecutar el script UT6P1_Parte2.ps1
+#     para continuar con la configuración de la estructura y creación de usuarios
+
+# ==================================================================================
+# ==================================================================================
+
+#     FIN DEL SCRIPT
+
+# Después del reinicio, debería aparecer el usuario EMPRESA\Administrador al inicar sesión
+
+# ==================================================================================
---
+++ UT6P1_Parte2.ps1
@@ -0,0 +1,100 @@
+﻿# UT6 - Práctica 1 --> Crear estructura AD con script de PowerShell
+# Izhar Sedano - 2º ASIR 2025-2026
+
+# Este script debe ejecutarse en 2 fases. Este es el script 2 de 2
+# Se debe hacer así ya que hay un reinicio obligatorio después de instalar AD
+
+# ==================================================================================
+
+#####      *****      SCRIPT 2      *****      #####
+
+# !!! SE DEBE EJECUTAR COMO ADMINISTRADOR !!! #
+
+# ==================================================================================
+
+# 2.1. Importar AD y declarar el DC y OU correspondiente
+Import-Module ActiveDirectory
+
+Write-Host "--- INICIANDO FASE 2: CREACIÓN DE ESTRUCTURA ---" -ForegroundColor Cyan
+
+$domain = "DC=empresa,DC=local"
+$rootOUName = "Empresa"
+$rootOUPath = "OU=$rootOUName,$domain"
+
+# ==================================================================================
+
+# 2.2. Importación de los CSV en variables para manejarlas a posterior:
+$RutaDepartamentos = "C:\Users\Administrador\Desktop\UT_Practica7_1_Archivos_necesarios\departamentos.csv"
+$RutaEmpleados = "C:\Users\Administrador\Desktop\UT_Practica7_1_Archivos_necesarios\empleados.csv"
+
+# ==================================================================================
+
+# 2.3. Creación de la OU Principal 'Empresa'
+Write-Host "Creando OU Principal..."
+if (-not (Get-ADOrganizationalUnit -Filter "Name -eq '$rootOUName'")) {
+    New-ADOrganizationalUnit -Name $rootOUName -Path $domain
+}
+
+# ==================================================================================
+
+# 2.4. Procesar Departamentos (CSV)
+$departamentos = Import-Csv -Path $RutaDepartamentos -Delimiter ";"
+
+# Para cada departamento, se ejecuta:
+foreach ($dep in $departamentos) {
+    $depName = $dep.departamento
+    Write-Host "Configurando departamento: $depName"
+    
+    # Crear OU del departamento
+    $ouDepPath = "OU=$depName,$rootOUPath"
+    New-ADOrganizationalUnit -Name $depName -Path $rootOUPath -Description $dep.descripcion
+    
+    # Crear Grupo Global
+    New-ADGroup -Name "Grupo_$depName" -GroupCategory Security -GroupScope Global -Path $ouDepPath
+}
+
+# ==================================================================================
+
+# 3. Procesar Empleados (CSV)
+$empleados = Import-Csv -Path $RutaEmpleados -Delimiter ";"
+
+# Para cada usuario se ejecutará;
+foreach ($emp in $empleados) {
+      # Formatear Login: nombre.apellido
+    $nombreLimpio = $emp.nombre.Replace(" ", "") # Elimina espacios de nombres
+    $apellidoLimpio = $emp.apellido.Replace(" ", "") # Elimina espacios de apellidos
+    $samAccount = "$($nombreLimpio.ToLower()).$($apellidoLimpio.ToLower())"
+    
+    $depEmp = $emp.departamento
+    $targetOU = "OU=$depEmp,$rootOUPath"
+    $pass = ConvertTo-SecureString "aso.2025" -AsPlainText -Force
+    
+    Write-Host "Creando usuario: $samAccount en $depEmp"
+    
+    # Crear Usuario
+    New-ADUser -Name "$($emp.nombre) $($emp.apellido)" `
+        -SamAccountName $samAccount `
+        -UserPrincipalName "$samAccount@empresa.local" `
+        -GivenName $emp.nombre `
+        -Surname $emp.apellido `
+        -Path $targetOU `
+        -AccountPassword $pass `
+        -ChangePasswordAtLogon $true `
+        -Enabled $true `
+        -ErrorAction SilentlyContinue
+        
+    # Vincular al Grupo
+    Add-ADGroupMember -Identity "Grupo_$depEmp" -Members $samAccount
+}
+
+Write-Host "PROCESO COMPLETADO EXITOSAMENTE."
+Write-Host ""
+Write-Host "Se recomienda volver a reiniciar en caso de ERROR"
+
+# ==================================================================================
+
+# FIN DEL SCRIPT 2
+
+# Después de ejecutar se podrá iniciar sesión en los usuarios creados y cambiar la contraseña
+
+# ==================================================================================
